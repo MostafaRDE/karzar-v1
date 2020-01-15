6e2d57ec27c0ddf94b74423aee47fe9d61a1e937
MostafaRDE <mostafa.rostamdokht@gmail.com>
2020-01-16 01:01:59 +0330

    Fix robots.txt path

--- server/router.js
+++ server/router.js
@@ -9,7 +9,7 @@ module.exports = (app) => {
     app.use(ApiUrl + 'tournaments' , require('./src/tournaments/Router'));
     app.use(ApiUrl + 'tutorials' , require('./src/tutorials/Router'));
     app.use(ApiUrl + 'uploads' , require('./src/uploads/Router'));
-    app.get('/robots.txt', (req, res) => {
+    app.get('/robots.txt', (req, res, next) => {
         let options = {
             root: path.join(__dirname, 'public'),
             dotfiles: 'deny',
