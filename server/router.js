3c7aa6531ba6eb6ea17fd54fa2a4163994dcb3ed
MostafaRDE <mostafa.rostamdokht@gmail.com>
2020-01-16 00:39:12 +0330

    Create robots.txt path

--- server/router.js
+++ server/router.js
@@ -7,5 +7,7 @@ module.exports = (app) => {
     app.use(ApiUrl + 'tournaments' , require('./src/tournaments/Router'));
     app.use(ApiUrl + 'tutorials' , require('./src/tutorials/Router'));
     app.use(ApiUrl + 'uploads' , require('./src/uploads/Router'));
-    app.use(ApiUrl + 'users' , require('./src/users/Router'));
+    app.get('/robots.txt', (req, res) => {
+        res.send('Disallow: *\nNoindex: *\n')
+    });
 };
